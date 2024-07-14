2d5ab4f4f24e2a0d4fc9b83c11e109e41f462fe1
YYBer <liyuanying225@gmail.com>
2024-07-14 05:41:55 +0200

    modify the extract_voiceprint

--- FVoice_Auth/backend/extract_voiceprint.py
+++ FVoice_Auth/backend/extract_voiceprint.py
@@ -1,22 +1,20 @@
 import librosa
 import numpy as np
+import base64
+import sys
 
 def extract_mfcc(file_path, n_mfcc=13):
-    # Load the audio file
     y, sr = librosa.load(file_path, sr=None)
-    
-    # Extract MFCC features
     mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
-    
-    # Compute the mean of the MFCC features
     mfccs_mean = np.mean(mfccs.T, axis=0)
-    
     return mfccs_mean
 
-# Usage example
 if __name__ == "__main__":
-    # file_path = "output4.wav"
-    file_path = sys.argv[1]
+    file_path = sys.argv[1]  # Accept file path as an argument
     mfcc_features = extract_mfcc(file_path)
-    np.save("voiceprint.npy", mfcc_features)
-    print(f"Extracted MFCC features: {mfcc_features}")
+    
+    # Convert the numpy array to a base64 string
+    mfcc_base64 = base64.b64encode(mfcc_features).decode('utf-8')
+    
+    # Print the base64 string to be captured by subprocess
+    print(mfcc_base64)
